324bbba6bfceebf5eb96a2270cd6c95a2d78a4e8
Swagat Ghimire <swagat.ghimire@hotmail.com>
2017-09-16 19:03:55 -0400

    resolved conflicts with front end code

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+import subprocess
 import json
 
 app = Flask(__name__)
@@ -14,6 +15,19 @@ def get_status():
     with open(STATUS_FILE, 'r') as infile:
         data = json.loads(infile)
     return data
+
+@app.route('/start_computation', methods=['POST'])
+def start_computation():
+    data = request.get_json()
+    validity = validate_articles(data['start_page'], data['end_page'])
+    if validity:
+        subprocess.popen("python3 wikipedia_game.py " + data['start_page']
+                         + " " + data['end_page'], + " -l " + data['max_turns'])
+        return True
+    else:
+        return False
+
+
 
 if __name__ == '__main__':
   app.run(port=80)
